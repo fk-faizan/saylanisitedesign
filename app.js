95229a595678c2c5bc5076547d17dc29c624aa2c
fk-faizan <faizankhn.fk97@gmail.com>
2020-05-07 00:10:25 +0500

    Saylani Website Design Assignment is complete

---
+++ app.js
@@ -0,0 +1,15 @@
+// $('.count').each(function () {
+//     $(this).prop('Counter',0).animate({
+//         Counter: $(this).text()
+//     }, {
+//         duration: 4000,
+//         easing: 'swing',
+//         step: function (now) {
+//             $(this).text(Math.ceil(now));
+//         }
+//     });
+// });
+
+// alert("Hello World!")
+
+// $(".count").counterUp({delay:10, time:1000});
